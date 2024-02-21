6dd9b23b4d365ca6aeca47765cf653facfe8be31
CejasClaudioA <cejasclaudioalejandro@gmail.com>
2024-02-20 21:15:19 -0300

    first commit

---
+++ src/main/java/com/dev/cejasclaudio/webappmascotas/WebappMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.dev.cejasclaudio.webappmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappMascotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappMascotasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/cejasclaudio/webappmascotas/WebappMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.cejasclaudio.webappmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappMascotasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
